6b88d4db5ea1b0c794d06c7cfeea1856acad7919
Caio Oliveira <caioinacio00@gmail.com>
2022-09-29 10:15:52 -0300

    gh pages

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import "./../node_modules/bootstrap/dist/js/bootstrap.min.js";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
 			<App />
 		</BrowserRouter>
 	</React.StrictMode>
