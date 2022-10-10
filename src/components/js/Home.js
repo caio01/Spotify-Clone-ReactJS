aa3b84fb59723e71d2ea30d921759fac9d00202a
Caio Oliveira <caioinacio00@gmail.com>
2022-10-10 16:11:54 -0300

    order by name

--- src/components/js/Home.js
+++ src/components/js/Home.js
@@ -10,6 +10,14 @@ function Home() {
 	useEffect(() => {getCollections.then(response => setCollections(response.data.results))}, []);
 	useEffect(() => {getPlaylists.then(response => setPlaylists(response.data.results))}, []);
 
+	console.log(
+		playlists
+		?.filter(w=>w.collections.filter(x=>x.id == 1).length > 0)
+		.sort(function(x,y){
+			let a = x.name.toUpperCase(), b = y.name.toUpperCase()
+			return a == b ? 0 : a > b ? 1 : -1
+			}))
+
 	const dataHTML = collections?.map((collection) => (
 		<div>
 			<div className="section-content-title">
@@ -21,7 +29,14 @@ function Home() {
 				</h3>
 			</div>
 			<div className="section-content-albuns">
-				{playlists?.filter(w=>w.collections.filter(x=>x.id == collection.id).length > 0).map(playlist=>
+				{playlists?.filter(w=>w.collections.filter(x=>x.id == collection.id).length > 0)
+				.sort(
+					function(x,y){
+						let a = x.name.toUpperCase(),
+							b = y.name.toUpperCase()
+						return a == b ? 0 : a > b ? 1 : -1
+					})
+				.map(playlist=>
 					<div className="section-content-albuns-album">
 						<Link to={`/singleplaylist/${collection.id}/${(playlist.id)}`} className="navbar-brand">
 							<img className="section-content-albuns-album-img" src={playlist.cover} alt="capa-album" />
