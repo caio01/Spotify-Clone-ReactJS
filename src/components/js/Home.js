0a3f44166ef0a72565dc8c31aae6c48ce3a1587e
Caio Oliveira <caioinacio00@gmail.com>
2022-10-17 20:09:27 -0300

    add modalUserSettings and modalCreatePlaylist

--- src/components/js/Home.js
+++ src/components/js/Home.js
@@ -1,17 +1,69 @@
-import "./../css/Home.css";
-import { Link } from "react-router-dom";
-import React, { useEffect, useState } from "react";
-import { getCollections, getPlaylists } from "./services/api.js"
+import "./../css/Home.css"
+import { Link } from "react-router-dom"
+import React, { useEffect, useState } from "react"
+import { getCollections, getPlaylists, getUser, updateUser, postPlaylist } from "./services/api"
+import { encrypt, decrypt } from "./services/crypt"
+import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 function Home() {
 	const [collections, setCollections] = useState();
 	const [playlists, setPlaylists] = useState();
 
-	useEffect(() => {getCollections.then(response => setCollections(response.data.results))}, []);
-	useEffect(() => {getPlaylists.then(response => setPlaylists(response.data.results))}, []);
+	const [users, setUsers] = useState();
+	const [id, setId] = useState('');
+	const [name, setName] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [dateBirth, setDateBirth] = useState('');
+	const [gender, setGender] = useState('');
+	const [modalTitle, setModalTitle] = useState('');
+	const [show, setShow] = useState(false);
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	const [namePlaylist, setNamePlaylist] = useState('');
+	const [descPlaylist, setDescPlaylist] = useState('');
+	const [coverPlaylist, setCoverPlaylist] = useState('');
+	const [musicsPlaylist, setMusicsPlaylist] = useState('');
+	const [showPlaylist, setShowPlaylist] = useState(false);
+	const handleClosePlaylist = () => setShowPlaylist(false);
+	const handleShowPlaylist = () => setShowPlaylist(true);
+
+	useEffect(() => {getUser.then(response => setUsers(response.data.results))}, [])
+	useEffect(() => {getCollections.then(response => setCollections(response.data.results))}, [])
+	useEffect(() => {getPlaylists.then(response => setPlaylists(response.data.results))}, [])
 
 	let user;
 	if(localStorage.length > 0) user = JSON.parse(localStorage.getItem("userLogged"))[0]
+
+	function handleSubmit(e) {
+		e.preventDefault()
+		
+		const data = {
+			"name": name,
+			"email": email,
+			"password": encrypt(password),
+			"dateBirth": dateBirth,
+			"gender": gender
+		}
+		updateUser(id, data)
+	}
+
+	function handleSubmitPlaylist(e) {
+		e.preventDefault()
+
+		var aux = (musicsPlaylist.split(',')).map(x=>(parseInt(x)))
+		
+		const data = {
+			"name": namePlaylist,
+			"desc": descPlaylist,
+			"cover": coverPlaylist,
+			"musics": aux,
+			"users": [user.id]
+		}
+		postPlaylist(data)
+	}
 
 	const dataHTML = collections?.map((collection) => (
 		<div>
@@ -87,8 +139,11 @@ function Home() {
 		return (
 			<div>
 				<div className="menu-user">
-					<p><Link to={"/listUsers"}>User Settings</Link></p>
-					<p><Link to={"/listPlaylists"}>New Playlist</Link></p>
+					<h3 onClick={()=>setModalTitle('Update User')&setId(user.id)&setName(user.name)&setEmail(user.email)
+									&setPassword(decrypt(user.password))&setDateBirth(user.dateBirth)&setGender(user.gender)}
+									onMouseUp={handleShow}>
+					User Settings</h3>
+					<h3 onClick={handleShowPlaylist}>New Playlist</h3>
 				</div>
 				<div className="body">
 					<section className="content">
@@ -96,6 +151,56 @@ function Home() {
 						{dataHTML}
 					</section>
 				</div>
+
+				<Modal show={show} onHide={handleClose} >
+					<form onSubmit={e => handleSubmit(e)}>
+						<Modal.Header closeButton>
+						<Modal.Title>{modalTitle}</Modal.Title>
+						</Modal.Header>
+						<Modal.Body>
+								<p>{"Id: " + id}</p>
+								<p>Name: <input type="text" value={name} onChange={(e)=>setName(e.target.value)}></input></p>
+								<p>Email: <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}></input></p>
+								<p>Password: <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)}></input></p>
+								<p>DateBirth: <input type="text" value={dateBirth} onChange={(e)=>setDateBirth(e.target.value)}></input></p>
+								<p>Gender: <input type="text" value={gender} onChange={(e)=>setGender(e.target.value)}></input></p>
+						</Modal.Body>
+						<Modal.Footer>
+						<Button variant="secondary" onClick={handleClose}>
+							Cancel
+						</Button>
+						<Button variant="primary" type="submit" onClick={handleClose}>
+							Save Changes
+						</Button>
+						</Modal.Footer>
+					</form>
+				</Modal>
+				
+
+				<Modal show={showPlaylist} onHide={handleClosePlaylist} >
+				<form onSubmit={e => handleSubmitPlaylist(e)}>
+					<Modal.Header closeButton>
+					<Modal.Title>{modalTitle}</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+							<h5>Playlist is private to {user.name}</h5>
+							<p>{"Id: "}</p>
+							<p>Name: <input type="text" value={namePlaylist} onChange={(e)=>setNamePlaylist(e.target.value)}></input></p>
+							<p>Desc: <input type="text" value={descPlaylist} onChange={(e)=>setDescPlaylist(e.target.value)}></input></p>
+							<p>Cover: <input type="text" value={coverPlaylist} onChange={(e)=>setCoverPlaylist(e.target.value)}></input></p>
+							<p>Id Musics: <input type="text" value={musicsPlaylist} onChange={(e)=>setMusicsPlaylist(e.target.value)}></input></p>
+					</Modal.Body>
+					<Modal.Footer>
+					<Button variant="secondary" onClick={handleClosePlaylist}>
+						Cancel
+					</Button>
+					<Button variant="primary" type="submit" onClick={handleClosePlaylist}>
+						Save Changes
+					</Button>
+					</Modal.Footer>
+				</form>
+			</Modal>
+
 			</div>
 		)
 	}
